a67b96de650c0e0ddc1abe1746820c0966ce57fb
Anurag Singh <anurag.prakash.singh@gmail.com>
2022-05-11 10:28:16 -0700

    Add unit test

--- src/test/java/org/anurag/houserobber3/TestHouseRobber3.java
+++ src/test/java/org/anurag/houserobber3/TestHouseRobber3.java
@@ -24,4 +24,22 @@ public class TestHouseRobber3 {
         Assertions.assertEquals(9, solution.rob(node1));
     }
 
+    @Test
+    void test2() {
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(4);
+        TreeNode node3 = new TreeNode(5);
+        TreeNode node4 = new TreeNode(8);
+        TreeNode node5 = new TreeNode(3);
+        TreeNode node6 = new TreeNode(1);
+        TreeNode node7 = new TreeNode(2);
+
+        node1.left = node2; node1.right = node3;
+        node2.left = node4; node2.right = node5;
+        node3.left = node6; node3.right = node7;
+
+        Solution solution = new Solution();
+
+        Assertions.assertEquals(17, solution.rob(node1));
+    }
 }
