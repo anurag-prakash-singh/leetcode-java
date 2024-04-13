067e7e09c6423737b981ca51a736a8ddc8041c16
Anurag Singh <anurag.prakash.singh@gmail.com>
2024-04-13 05:22:24 -0700

    Count zero request servers - optimized

--- src/main/java/org/anurag/countzerorequestservers/Solution.java
+++ src/main/java/org/anurag/countzerorequestservers/Solution.java
@@ -1,2 +1,168 @@
-package org.anurag.countzerorequestservers;public class Solution {
+package org.anurag.countzerorequestservers;
+
+import java.util.*;
+
+public class Solution {
+
+    static class ServerRequestLog {
+        int serverId;
+        int time;
+
+        ServerRequestLog(int serverId, int time) {
+            this.serverId = serverId;
+            this.time = time;
+        }
+    }
+
+    static class QueryAndIndex {
+        int query;
+        int index;
+
+        QueryAndIndex(int query, int index) {
+            this.query = query;
+            this.index = index;
+        }
+    }
+
+    // Binary search for the index corresponding to the start of logs with timestamp.
+    static int startingIndexOfLogTimestamp(ServerRequestLog[] serverRequestLogs, int timestamp) {
+        int start = 0;
+        int end = serverRequestLogs.length - 1;
+        int mid = (start + end) / 2;
+
+        if (timestamp > serverRequestLogs[end].time) {
+            // timestamp is too far ahead in to the future. There are no logs starting at this time.
+            return -1;
+        }
+
+        while (start <= end) {
+            mid = (start + end) / 2;
+            ServerRequestLog logAtMid = serverRequestLogs[mid];
+
+            if (logAtMid.time >= timestamp) {
+                if (mid - 1 < start || serverRequestLogs[mid -1].time < timestamp) {
+                    return mid;
+                }
+
+                end = mid - 1;
+                continue;
+            }
+
+            // mid ts < timestamp; search right
+            start = mid + 1;
+        }
+
+        return mid;
+    }
+
+    public int[] countServers(int n, int[][] logs, int x, int[] queries) {
+        int[] result = new int[queries.length];
+        ServerRequestLog[] serverRequestLogs = new ServerRequestLog[logs.length];
+
+        // Copy logs into serverRequestLogs and sort serverRequestLogs
+        for (int i = 0; i < logs.length; i++) {
+            serverRequestLogs[i] = new ServerRequestLog(logs[i][0], logs[i][1]);
+        }
+
+        QueryAndIndex[] queryAndIndices = new QueryAndIndex[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            queryAndIndices[i] = new QueryAndIndex(queries[i], i);
+        }
+
+        Arrays.sort(serverRequestLogs, Comparator.comparing(s -> s.time));
+        Arrays.sort(queryAndIndices, Comparator.comparing(q -> q.query));
+        Arrays.sort(queries);
+
+        int wStart = -1, wEnd = -1;
+        Set<Integer> zeroRequestCountServers = new HashSet<>();
+        Map<Integer, Integer> serverRequestCounts = new Hashtable<>();
+
+        for (int qIndex = 0; qIndex < queryAndIndices.length; qIndex++) {
+            int qEnd = queryAndIndices[qIndex].query;
+            int qStart = qEnd - x;
+
+            while (wEnd < serverRequestLogs.length && (wEnd == -1 || serverRequestLogs[wEnd].time <= qEnd)) {
+                if (wEnd > -1) {
+                    int serverId = serverRequestLogs[wEnd].serverId;
+
+                    if (!serverRequestCounts.containsKey(serverId)) {
+                        serverRequestCounts.put(serverId, 1);
+                    } else {
+                        int requestCountForServer = serverRequestCounts.get(serverId);
+                        serverRequestCounts.put(serverId, requestCountForServer + 1);
+                    }
+
+                    zeroRequestCountServers.remove(serverId);
+                }
+
+                wEnd++;
+            }
+
+            while (wStart < wEnd && (wStart == -1 || serverRequestLogs[wStart].time < qStart)) {
+                if (wStart > -1) {
+                    int serverId = serverRequestLogs[wStart].serverId;
+                    int requestCountForServer = serverRequestCounts.get(serverId);
+                    requestCountForServer--;
+                    serverRequestCounts.put(serverId, requestCountForServer);
+
+                    if (requestCountForServer == 0) {
+                        zeroRequestCountServers.add(serverId);
+                        serverRequestCounts.remove(serverId);
+                    }
+                }
+
+                wStart++;
+            }
+
+            result[queryAndIndices[qIndex].index] = n - serverRequestCounts.size();
+        }
+
+        return result;
+    }
+
+    public int[] countServersBinarySearch(int n, int[][] logs, int x, int[] queries) {
+        int[] result = new int[queries.length];
+        ServerRequestLog[] serverRequestLogs = new ServerRequestLog[logs.length];
+
+        // Copy logs into serverRequestLogs and sort serverRequestLogs
+        for (int i = 0; i < logs.length; i++) {
+            serverRequestLogs[i] = new ServerRequestLog(logs[i][0], logs[i][1]);
+        }
+
+        Arrays.sort(serverRequestLogs, Comparator.comparing(s -> s.time));
+
+        int resultIndex = 0;
+        for (int q : queries) {
+            int startingTs = q - x;
+            int endingTs = q + 1;
+
+            int startingTsIndex = startingIndexOfLogTimestamp(serverRequestLogs, startingTs);
+            int endingTsIndex = startingIndexOfLogTimestamp(serverRequestLogs, endingTs);
+
+            if (startingTsIndex == -1) {
+                // No server served requests starting at this query TS
+                result[resultIndex++] = n;
+                continue;
+            }
+
+            if (endingTsIndex == -1) {
+                endingTsIndex = serverRequestLogs.length - 1;
+            } else {
+                endingTsIndex = Math.max(startingTsIndex, endingTsIndex - 1);
+            }
+
+            Set<Integer> serversWithRequests = new HashSet<>();
+
+            for (int l = startingTsIndex; l <= endingTsIndex; l++) {
+                if (serverRequestLogs[l].time >= startingTs && serverRequestLogs[l].time < endingTs) {
+                    serversWithRequests.add(serverRequestLogs[l].serverId);
+                }
+            }
+
+            result[resultIndex++] = n - serversWithRequests.size();
+        }
+
+        return result;
+    }
+
 }
--- src/test/java/org/anurag/countzerorequestservers/TestCountZeroRequestServers.java
+++ src/test/java/org/anurag/countzerorequestservers/TestCountZeroRequestServers.java
@@ -1,2 +1,47 @@
-package org.anurag.countzerorequestservers;public class TestCountZeroRequestServers {
+package org.anurag.countzerorequestservers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCountZeroRequestServers {
+
+    @Test
+    public void test1() {
+        Solution solution = new Solution();
+        int[] result = solution.countServers(3, new int[][]{new int[]{1,3}, new int[]{2,6}, new int[]{1,5}}, 5, new int[]{10, 11});
+
+        Assertions.assertArrayEquals(new int[]{1,2}, result);
+    }
+
+    @Test
+    public void test2() {
+        Solution solution = new Solution();
+        int[] result = solution.countServers(3, new int[][]{new int[]{2,4}, new int[]{2,1}, new int[]{1,2}, new int[]{3,1}}, 2, new int[]{3,4});
+
+        Assertions.assertArrayEquals(new int[]{0,1}, result);
+    }
+
+    @Test
+    public void test3() {
+        Solution solution = new Solution();
+        int[] result = solution.countServers(4, new int[][]{new int[]{2,30}, new int[]{2,5}, new int[]{3,9}, new int[]{4,21}}, 9, new int[]{11});
+
+        Assertions.assertArrayEquals(new int[]{2}, result);
+    }
+
+    @Test
+    public void test4() {
+        Solution solution = new Solution();
+        int[] result = solution.countServers(6, new int[][]{new int[]{1,21}}, 10, new int[]{24,20});
+
+        Assertions.assertArrayEquals(new int[]{5,6}, result);
+    }
+
+    @Test
+    public void test5() {
+        Solution solution = new Solution();
+        int[] result = solution.countServers(6, new int[][]{new int[]{1,21}}, 10, new int[]{24,35,28,26,20,25,16,31,12});
+
+        Assertions.assertArrayEquals(new int[]{5,6,5,5,6,5,6,5,6}, result);
+    }
 }
